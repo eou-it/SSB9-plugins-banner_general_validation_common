7087f3ed6e4986ddffada20a05bbf93829133c6b
Rajesh Kumar <rajesh.kumar@sungardhe.com>
2011-09-14 15:39:56 -0400

    Update for copyright

--- grails-app/domain/com/sungardhe/banner/general/system/FunctionRevenue.groovy
+++ grails-app/domain/com/sungardhe/banner/general/system/FunctionRevenue.groovy
@@ -5,9 +5,10 @@
  SunGard Higher Education and its subsidiaries. Any use of this software is limited 
  solely to SunGard Higher Education licensees, and is further subject to the terms 
  and conditions of one or more written license agreements between SunGard Higher 
- Education and the licensee in question. SunGard, Banner and Luminis are either 
- registered trademarks or trademarks of SunGard Higher Education in the U.S.A. 
- and/or other regions and/or countries.
+ Education and the licensee in question. SunGard is either a registered trademark or
+ trademark of SunGard Data Systems in the U.S.A. and/or other regions and/or countries.
+ Banner and Luminis are either registered trademarks or trademarks of SunGard Higher 
+ Education in the U.S.A. and/or other regions and/or countries.
  **********************************************************************************/
 /**
  Banner Automator Version: 1.24
--- src/ddl/dbeu_ext_gen_bgvc.sql
+++ src/ddl/dbeu_ext_gen_bgvc.sql
@@ -3,16 +3,19 @@
 --
 -- V8.1
 --
--- **************************************************************************************
--- * Copyright 2009-2011 SunGard Higher Education. All Rights Reserved.                 *
--- * This copyrighted software contains confidential and proprietary information of     *
--- * SunGard Higher Education and its subsidiaries. Any use of this software is limited *
--- * solely to SunGard Higher Education licensees, and is further subject to the terms  *
--- * and conditions of one or more written license agreements between SunGard Higher    *
--- * Education and the licensee in question. SunGard, Banner and Luminis are either     *
--- * registered trademarks or trademarks of SunGard Higher Education in the U.S.A.      *
--- * and/or other regions and/or countries.                                             *
--- **************************************************************************************
+-- *****************************************************************************************
+-- * Copyright 2009-2011 SunGard Higher Education. All Rights Reserved.                    *
+-- * This copyrighted software contains confidential and proprietary information of        *
+-- * SunGard Higher Education and its subsidiaries. Any use of this software is limited    *
+-- * solely to SunGard Higher Education licensees, and is further subject to the terms     *
+-- * and conditions of one or more written license agreements between SunGard Higher       *
+-- * Education and the licensee in question. SunGard is either a registered trademark or   *
+-- * trademark of SunGard Data Systems in the U.S.A. and/or other regions and/or countries.*
+-- * Banner and Luminis are either registered trademarks or trademarks of SunGard Higher   *
+-- * Education in the U.S.A. and/or other regions and/or countries.                        *
+-- *****************************************************************************************
+
+
 --
 whenever oserror exit rollback;
 whenever sqlerror exit rollback;

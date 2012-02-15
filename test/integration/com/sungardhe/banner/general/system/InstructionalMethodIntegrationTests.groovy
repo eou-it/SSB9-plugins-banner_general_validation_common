74968bebb5161ae0f936d6b85a406f58557272ca
suresh.babu <suresh.babu@sungard.com>
2012-02-15 18:48:22 +0530

    .size is changed to .size()

--- test/integration/com/sungardhe/banner/general/system/InstructionalMethodIntegrationTests.groovy
+++ test/integration/com/sungardhe/banner/general/system/InstructionalMethodIntegrationTests.groovy
@@ -1,12 +1,12 @@
 /*********************************************************************************
  Copyright 2009-2011 SunGard Higher Education. All Rights Reserved.
- This copyrighted software contains confidential and proprietary information of 
- SunGard Higher Education and its subsidiaries. Any use of this software is limited 
- solely to SunGard Higher Education licensees, and is further subject to the terms 
- and conditions of one or more written license agreements between SunGard Higher 
+ This copyrighted software contains confidential and proprietary information of
+ SunGard Higher Education and its subsidiaries. Any use of this software is limited
+ solely to SunGard Higher Education licensees, and is further subject to the terms
+ and conditions of one or more written license agreements between SunGard Higher
  Education and the licensee in question. SunGard is either a registered trademark or
  trademark of SunGard Data Systems in the U.S.A. and/or other regions and/or countries.
- Banner and Luminis are either registered trademarks or trademarks of SunGard Higher 
+ Banner and Luminis are either registered trademarks or trademarks of SunGard Higher
  Education in the U.S.A. and/or other regions and/or countries.
  **********************************************************************************/
 package com.sungardhe.banner.general.system;
@@ -59,7 +59,7 @@ class InstructionalMethodIntegrationTests extends BaseIntegrationTestCase {
     void testFindAllByCode() {
         saveInstructionalMethod()
         def imcs = InstructionalMethod.findAllByCode("1111")
-        assertTrue imcs.size > 0
+        assertTrue imcs.size() > 0
     }
 
 
@@ -73,7 +73,7 @@ class InstructionalMethodIntegrationTests extends BaseIntegrationTestCase {
     void testFindAllByVoiceResponseMessageNumber() {
         saveInstructionalMethod()
         def imcs = InstructionalMethod.findAllByVoiceResponseMessageNumber(new BigDecimal(22))
-        assertTrue imcs.size > 0
+        assertTrue imcs.size() > 0
     }
 
 

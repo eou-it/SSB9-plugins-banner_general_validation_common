c3f5e61675c2ab510bbe7803827896c72cfcfe11
Jim Caley <jim.caley@ellucian.com>
2015-09-17 10:55:05 -0400

    Add entity for GXVDIRD

---
+++ src/groovy/net/hedtech/banner/general/crossproduct/BankRoutingInfo.groovy
@@ -0,0 +1,81 @@
+/*******************************************************************************
+Copyright 2015 Ellucian Company L.P. and its affiliates.
+*******************************************************************************/
+package net.hedtech.banner.general.crossproduct
+
+import javax.persistence.*
+ 
+/**
+ * Bank Routing Number Table entity.
+ */
+@Entity
+@Table(name = "GXVDIRD")
+class BankRoutingInfo implements Serializable {
+
+    /**
+     * Surrogate ID for GXVDIRD
+     */
+    @Column(name = "GXVDIRD_SURROGATE_ID")
+    @SequenceGenerator(name = "GXVDIRD_SEQ_GEN", allocationSize = 1, sequenceName = "GXVDIRD_SURROGATE_ID_SEQUENCE")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "GXVDIRD_SEQ_GEN")
+    Long id
+
+    /**
+     * VERSION: Optimistic lock token.
+     */
+    @Version
+    @Column(name = "GXVDIRD_VERSION")
+    Long version
+
+    /**
+     * BANK ROUTING NUMBER:  Routing number for bank.
+     * Id annotation is here to allow joins on this field with GXRDIRD.
+     */
+    @Id
+    @Column(name = "GXVDIRD_CODE_BANK_ROUT_NUM")
+    String bankRoutingNum
+
+    /**
+     * BANK NAME:  Description contains name of bank.
+     */
+    @Column(name = "GXVDIRD_DESC")
+    String bankName
+
+    /**
+     * ACTIVITY DATE: The date when the information for this record on the table was entered or last updated.
+     */
+    @Column(name = "GXVDIRD_ACTIVITY_DATE")
+    @Temporal(TemporalType.TIMESTAMP)
+    Date lastModified
+
+    /**
+     * USER ID: The user id of the person who updates this record.
+     */
+    @Column(name = "GXVDIRD_USER_ID")
+    String lastModifiedBy
+
+    /**
+     * DATA ORIGIN: Source system that created or updated the data.
+     */
+    @Column(name = "GXVDIRD_DATA_ORIGIN")
+    String dataOrigin
+
+    public String toString() {
+        """BankRoutingInfo [
+            id= $id,
+            version= $version,
+            bankRoutingNum= $bankRoutingNum,
+            bankName= $bankName,
+            lastModified= $lastModified,
+            lastModifiedBy= $lastModifiedBy,
+            dataOrigin= $dataOrigin]"""
+    }
+
+    static constraints = {
+        bankRoutingNum(nullable: false, maxSize: 11)
+        bankName(nullable: false, maxSize: 60)
+        lastModified(nullable: true)
+        lastModifiedBy(nullable: true, maxSize: 30)
+        dataOrigin(nullable: true, maxSize: 30)
+    }
+}

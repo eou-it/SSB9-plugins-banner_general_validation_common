d128a0e56a1db706c931ed3513c1d4ac7fe4bac9
Venu Thanneeru <venu.thanneeru@ellucian.com>
2015-12-14 18:27:04 +0530

    BSAPI-142 & BSAPI-144 : code review comments are incorporated

---
+++ grails-app/services/net/hedtech/banner/general/common/GeneralValidationCommonConstants.groovy
@@ -0,0 +1,24 @@
+/*********************************************************************************
+ Copyright 2015 Ellucian Company L.P. and its affiliates.
+ **********************************************************************************/
+
+package net.hedtech.banner.general.common
+
+/**
+ * Util class for General validation module where in  will be
+ * This class will holds all common variables used across the banner-general-validation-common plugin.
+ */
+
+class GeneralValidationCommonConstants {
+
+    private GeneralValidationCommonConstants() {} // prevents instantiation
+
+    /** Common constants */
+    final static String TYPE = 'type'
+    final static String CODE = 'code'
+    final static String ERROR_MSG_EXISTS_MESSAGE = 'exists.message'
+    final static String ERROR_MSG_CODE_REQUIRED = 'code.required.message'
+
+    /** Common constants for AcademicDiscipline*/
+    final static String ACADEMIC_DISCIPLINE = 'academicDiscipline'
+}
--- grails-app/services/net/hedtech/banner/general/overall/ldm/GlobalUniqueIdentifierService.groovy
+++ grails-app/services/net/hedtech/banner/general/overall/ldm/GlobalUniqueIdentifierService.groovy
@@ -1,5 +1,5 @@
 /*******************************************************************************
- Copyright 2014 Ellucian Company L.P. and its affiliates.
+ Copyright 2014-2015 Ellucian Company L.P. and its affiliates.
  *******************************************************************************/
 package net.hedtech.banner.general.overall.ldm
 import net.hedtech.banner.service.ServiceBase
@@ -11,4 +11,23 @@ class GlobalUniqueIdentifierService extends ServiceBase {
 
     public static final String API ="api"
 
+    /**
+     * fetch GlobalUniqueIdentifier info based on ldmName and guid
+     * @param ldmName
+     * @param guid
+     * @return GlobalUniqueIdentifier
+     */
+     GlobalUniqueIdentifier fetchByLdmNameAndGuid(String ldmName, String guid) {
+       return  GlobalUniqueIdentifier.fetchByLdmNameAndGuid(ldmName, guid)
+    }
+
+    /**
+     * fetch GlobalUniqueIdentifier info based on ldmName and domainId
+     * @param ldmName
+     * @param domainId
+     * @return GlobalUniqueIdentifier
+     */
+    GlobalUniqueIdentifier  fetchByLdmNameAndDomainId(String ldmName, Long domainId){
+        return  GlobalUniqueIdentifier.fetchByLdmNameAndDomainId(ldmName, domainId)
+    }
 }

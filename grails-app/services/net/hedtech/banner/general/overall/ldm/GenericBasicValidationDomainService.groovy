df4b7c9c703a53f8a47bfd40bba4f99586054235
Vijay Kumar Tiwari <vijay.tiwari@ellucian.com>
2016-05-31 16:30:37 +0530

    BAPI-5836: added generic validation service and integration tests

---
+++ grails-app/services/net/hedtech/banner/general/overall/ldm/GenericBasicValidationDomainService.groovy
@@ -0,0 +1,200 @@
+/*******************************************************************************
+ Copyright 2014-2015 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
+package net.hedtech.banner.general.overall.ldm
+
+import groovy.transform.ToString
+import net.hedtech.banner.exceptions.ApplicationException
+import net.hedtech.banner.query.DynamicFinder
+import net.hedtech.banner.query.QueryBuilder
+import net.hedtech.banner.query.operators.Operators
+import net.hedtech.banner.restfulapi.RestfulApiValidationUtility
+
+/**
+ * Generic service method for basic validation tables used by HEDM
+ */
+@ToString(includeFields = true)
+class GenericBasicValidationDomainService {
+    Class baseDomain
+    String baseDomainKeyField
+    Class guidDomain
+    String guidIdField
+    String guidDomainKeyField
+    String ldmNameField
+    String ldmNameValue
+    Class decorator
+    List supportedSearchFields
+    List supportedSortFields
+    Map ethosToDomainFieldNameMap
+    String defaultSortField
+    Integer DEFAULT_PAGE_SIZE = 500
+    Integer MAX_PAGE_SIZE = 1000
+    private static
+    final List<String> grailsDynamicPropeties = ['attached', 'domainClass', 'class', 'constraints', 'dirtyPropertyNames', 'errors', 'dirty', 'metadata']
+
+    def get(guid) {
+        log.info("Begin get for id:${guid} with setup:${this.toString()}")
+        def decoratedResponse;
+        validateSettings()
+        if (baseDomain == guidDomain) {
+            //base table has guid field in it we will not join
+            String query = """FROM ${guidDomain.getName()}
+                                WHERE ${ldmNameField} = '${ldmNameValue}'
+                                AND ${guidIdField} = (:guid)"""
+            def response = guidDomain.executeQuery(query, [guid: guid])[0]
+            Map properties = response.properties
+            removeGrailsProperties(properties)
+            decoratedResponse = decorator.newInstance()
+            properties.keySet().each {
+                if (decoratedResponse.hasProperty(it)) {
+                    decoratedResponse."${it}" = properties.get(it)
+                }
+            }
+        } else {
+            //we will have to perform join
+            String query = """  FROM ${baseDomain.getName()} a, ${guidDomain.getName()} b
+                                WHERE a.${baseDomainKeyField} = b.${guidDomainKeyField}
+                                AND b.${ldmNameField} = '${ldmNameValue}'
+                                AND b.${guidIdField} = (:guid)"""
+            def response = guidDomain.executeQuery(query, [guid: guid])[0]
+            Map properties = response[0].properties
+            properties.putAll(response[1].properties)
+            removeGrailsProperties(properties)
+            decoratedResponse = decorator.newInstance()
+            properties.keySet().each {
+                if (decoratedResponse.hasProperty(it)) {
+                    decoratedResponse."${it}" = properties.get(it)
+                }
+            }
+        }
+        log.info("End get for id:${guid}")
+        return decoratedResponse
+    }
+
+    def list(Map params) {
+        log.info("Begin list with params:${params} with setup:${this.toString()}")
+        def decoratedResponse;
+        def listResponse = fetchForListOrCount(params)
+        if (baseDomain == guidDomain) {
+            List decoratedListResponse = []
+            listResponse.each { response ->
+                Map properties = response.properties
+                removeGrailsProperties(properties)
+                decoratedResponse = decorator.newInstance()
+                properties.keySet().each {
+                    if (decoratedResponse.hasProperty(it)) {
+                        decoratedResponse."${it}" = properties.get(it)
+                    }
+                }
+                decoratedListResponse << decoratedResponse
+            }
+            return decoratedListResponse
+        } else {
+            List decoratedListResponse = []
+            listResponse.each { response ->
+                Map properties = response[0].properties
+                properties.putAll(response[1].properties)
+                removeGrailsProperties(properties)
+                decoratedResponse = decorator.newInstance()
+                properties.keySet().each {
+                    if (decoratedResponse.hasProperty(it)) {
+                        decoratedResponse."${it}" = properties.get(it)
+                    }
+                }
+                decoratedListResponse << decoratedResponse
+            }
+            return decoratedListResponse
+        }
+        log.info("End list with params:${params}")
+    }
+
+    def count(params) {
+        return fetchForListOrCount(params, true)
+    }
+
+    private def fetchForListOrCount(Map params, boolean count = false) {
+        RestfulApiValidationUtility.correctMaxAndOffset(params, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE)
+        RestfulApiValidationUtility.validateSortField(params.sort, supportedSortFields)
+        RestfulApiValidationUtility.validateSortOrder(params.order)
+        Map pagingAndSortParams = params.clone()
+        pagingAndSortParams.sort = ethosToDomainFieldNameMap && ethosToDomainFieldNameMap.get(pagingAndSortParams.sort) ?
+                ethosToDomainFieldNameMap.get(pagingAndSortParams.sort) : pagingAndSortParams.sort
+        pagingAndSortParams.sort = pagingAndSortParams.sort ?: defaultSortField
+        pagingAndSortParams.order = pagingAndSortParams.order ?: 'ASC'
+        if (baseDomain == guidDomain) {
+            //base table has guid field in it we will not join
+            String query = """FROM ${guidDomain.getName()} a"""
+            DynamicFinder df = new DynamicFinder(guidDomain, query, "a")
+            Map queryParams = [:]
+            List criteria = []
+            supportedSearchFields.each { field ->
+                if (params.containsKey(field)) {
+                    queryParams.put(field, params.get(field))
+                    criteria.add([key: field, binding: ethosToDomainFieldNameMap?.get(field) ?: field, operator: Operators.EQUALS])
+                }
+            }
+            if (count) {
+                return df.count([params: queryParams, criteria: criteria])
+            } else {
+                return df.find([params: queryParams, criteria: criteria], QueryBuilder.getFilterData(pagingAndSortParams).pagingAndSortParams)
+            }
+        } else {
+            //we will have to perform join
+            String query = """  FROM ${baseDomain.getName()} a, ${guidDomain.getName()} b
+                                WHERE a.${baseDomainKeyField} = b.${guidDomainKeyField}
+                                AND b.${ldmNameField} = '${ldmNameValue}'"""
+            DynamicFinder df = new DynamicFinder(baseDomain, query, "a")
+            Map queryParams = [:]
+            List criteria = []
+            supportedSearchFields.each { field ->
+                if (params.containsKey(field)) {
+                    queryParams.put(field, params.get(field))
+                    criteria.add([key: field, binding: ethosToDomainFieldNameMap?.get(field) ?: field, operator: Operators.EQUALS])
+                }
+            }
+            if (count) {
+                return df.count([params: queryParams, criteria: criteria])
+            } else {
+                return df.find([params: queryParams, criteria: criteria], QueryBuilder.getFilterData(pagingAndSortParams).pagingAndSortParams)
+            }
+        }
+    }
+
+    protected void validateSettings() {
+        if (baseDomain == null) {
+            throw new ApplicationException(baseDomain, "required.baseDomain")
+        }
+
+        if (guidDomain == null) {
+            throw new ApplicationException(guidDomain, "required.guidDomain")
+        }
+
+        if (decorator == null) {
+            throw new ApplicationException(decorator, "required.decorator")
+        }
+
+        if (!guidIdField) {
+            throw new ApplicationException(guidDomain, "required.guidIdField")
+        }
+        if (!ldmNameField) {
+            throw new ApplicationException(guidDomain, "required.ldmNameField")
+        }
+        if (!ldmNameValue) {
+            throw new ApplicationException(guidDomain, "required.ldmNameValue")
+        }
+        if (baseDomain != guidDomain) {
+            if (!baseDomainKeyField) {
+                throw new ApplicationException(guidDomain, "required.baseDomainKeyField")
+            }
+            if (!guidDomainKeyField) {
+                throw new ApplicationException(guidDomain, "required.guidDomainKeyField")
+            }
+        }
+    }
+
+    private void removeGrailsProperties(Map properties) {
+        grailsDynamicPropeties.each {
+            properties.remove(it)
+        }
+    }
+}
---
+++ test/integration/net/hedtech/banner/general/overall/ldm/GenericBasicValidationDomainServiceIntegrationTests.groovy
@@ -0,0 +1,237 @@
+package net.hedtech.banner.general.overall.ldm
+
+import net.hedtech.banner.exceptions.ApplicationException
+import net.hedtech.banner.general.system.Level
+import net.hedtech.banner.general.system.ldm.v1.AcademicLevel
+import net.hedtech.banner.testing.BaseIntegrationTestCase
+import org.junit.Before
+import org.junit.Test
+import org.springframework.orm.hibernate3.HibernateQueryException
+
+class GenericBasicValidationDomainServiceIntegrationTests extends BaseIntegrationTestCase {
+
+    def genericBasicValidationDomainService
+
+    @Before
+    public void setUp() {
+        formContext = ['GUAGMNU']
+        super.setUp()
+        genericBasicValidationDomainService.baseDomain = null
+        genericBasicValidationDomainService.baseDomainKeyField = null
+        genericBasicValidationDomainService.guidDomain = null
+        genericBasicValidationDomainService.guidIdField = null
+        genericBasicValidationDomainService.guidDomainKeyField = null
+        genericBasicValidationDomainService.ldmNameField = null
+        genericBasicValidationDomainService.ldmNameValue = null
+        genericBasicValidationDomainService.decorator = null
+        genericBasicValidationDomainService.supportedSearchFields = null
+        genericBasicValidationDomainService.supportedSortFields = null
+        genericBasicValidationDomainService.ethosToDomainFieldNameMap = null
+        genericBasicValidationDomainService.defaultSortField = null
+    }
+
+    @Test
+    void testGetGuidInSameDomain() {
+        GlobalUniqueIdentifier globalUniqueIdentifier = GlobalUniqueIdentifier.findAllByLdmName('section-grade-types')[0]
+        genericBasicValidationDomainService.baseDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.guidDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.decorator = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.guidIdField = 'guid'
+        genericBasicValidationDomainService.ldmNameField = 'ldmName'
+        genericBasicValidationDomainService.ldmNameValue = 'section-grade-types'
+        GlobalUniqueIdentifier response = genericBasicValidationDomainService.get(globalUniqueIdentifier.guid)
+        assertNotNull(response)
+        assertEquals(globalUniqueIdentifier.guid, response.guid)
+    }
+
+    @Test
+    void testGetGuidInDifferentDomain() {
+        GlobalUniqueIdentifier globalUniqueIdentifier = GlobalUniqueIdentifier.findAllByLdmName('academic-levels')[0]
+        genericBasicValidationDomainService.baseDomain = Level.class
+        genericBasicValidationDomainService.guidDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.decorator = AcademicLevel.class
+        genericBasicValidationDomainService.guidIdField = 'guid'
+        genericBasicValidationDomainService.guidDomainKeyField = 'domainKey'
+        genericBasicValidationDomainService.ldmNameField = 'ldmName'
+        genericBasicValidationDomainService.ldmNameValue = 'academic-levels'
+        genericBasicValidationDomainService.baseDomainKeyField = 'code'
+        AcademicLevel response = genericBasicValidationDomainService.get(globalUniqueIdentifier.guid)
+        assertNotNull(response)
+        assertEquals(globalUniqueIdentifier.domainKey, response.code)
+        assertEquals(globalUniqueIdentifier.guid, response.guid)
+    }
+
+    @Test
+    void testValidateSettingsNullBaseDomain() {
+        genericBasicValidationDomainService.baseDomain = null
+        genericBasicValidationDomainService.guidDomain = GlobalUniqueIdentifier.class
+        shouldFail(ApplicationException) {
+            genericBasicValidationDomainService.get(1)
+        }
+    }
+
+    @Test
+    void testValidateSettingsInvalidBaseDomain() {
+        genericBasicValidationDomainService.baseDomain = ArrayList.class
+        genericBasicValidationDomainService.guidDomain = GlobalUniqueIdentifier.class
+        shouldFail(ApplicationException) {
+            genericBasicValidationDomainService.get(1)
+        }
+    }
+
+    @Test
+    void testValidateSettingsNullGuidDomain() {
+        genericBasicValidationDomainService.baseDomain = Level.class
+        genericBasicValidationDomainService.guidDomain = null
+        shouldFail(ApplicationException) {
+            genericBasicValidationDomainService.get(1)
+        }
+    }
+
+    @Test
+    void testValidateSettingsInvalidGuidDomain() {
+        genericBasicValidationDomainService.baseDomain = Level.class
+        genericBasicValidationDomainService.guidDomain = ArrayList.class
+        shouldFail(ApplicationException) {
+            genericBasicValidationDomainService.get(1)
+        }
+    }
+
+    @Test
+    void testValidateSettingsNullDecorator() {
+        genericBasicValidationDomainService.baseDomain = Level.class
+        genericBasicValidationDomainService.guidDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.decorator = null
+        shouldFail(ApplicationException) {
+            genericBasicValidationDomainService.get(1)
+        }
+    }
+
+    @Test
+    void testValidateSettingsNullGuidIdFieldDomain() {
+        genericBasicValidationDomainService.baseDomain = Level.class
+        genericBasicValidationDomainService.guidDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.decorator = Level.class
+        genericBasicValidationDomainService.guidIdField = null
+        shouldFail(ApplicationException) {
+            genericBasicValidationDomainService.get(1)
+        }
+    }
+
+    @Test
+    void testListSameDomainNoFilters() {
+        genericBasicValidationDomainService.baseDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.guidDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.decorator = GlobalUniqueIdentifier.class
+        List<GlobalUniqueIdentifier> globalUniqueIdentifierList = genericBasicValidationDomainService.list([:])
+        assertNotNull(globalUniqueIdentifierList)
+        assertEquals(genericBasicValidationDomainService.DEFAULT_PAGE_SIZE, globalUniqueIdentifierList.size())
+    }
+
+    @Test
+    void testListSameDomainNoFiltersWithMax() {
+        genericBasicValidationDomainService.baseDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.guidDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.decorator = GlobalUniqueIdentifier.class
+        List<GlobalUniqueIdentifier> globalUniqueIdentifierList = genericBasicValidationDomainService.list([max: "100"])
+        assertNotNull(globalUniqueIdentifierList)
+        assertEquals(100, globalUniqueIdentifierList.size())
+    }
+
+    @Test
+    void testListSameDomainNoFiltersWithMaxMoreThanMaxAllowed() {
+        genericBasicValidationDomainService.baseDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.guidDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.decorator = GlobalUniqueIdentifier.class
+        List<GlobalUniqueIdentifier> globalUniqueIdentifierList = genericBasicValidationDomainService.list([max: "2000"])
+        assertNotNull(globalUniqueIdentifierList)
+        assertEquals(genericBasicValidationDomainService.MAX_PAGE_SIZE, globalUniqueIdentifierList.size())
+    }
+
+    @Test
+    void testListSameDomainFilterNotListedAsAllowed() {
+        genericBasicValidationDomainService.baseDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.guidDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.defaultSortField = 'id'
+        genericBasicValidationDomainService.decorator = GlobalUniqueIdentifier.class
+        List<GlobalUniqueIdentifier> globalUniqueIdentifierList = genericBasicValidationDomainService.list([ldmName: 'persons'])
+        assertNotNull(globalUniqueIdentifierList)
+        List<GlobalUniqueIdentifier> globalUniqueIdentifierList1 = genericBasicValidationDomainService.list([:])
+        assertEquals(globalUniqueIdentifierList.id.sort(), globalUniqueIdentifierList1.id.sort())
+    }
+
+    @Test
+    void testListSameDomainFiltered() {
+        genericBasicValidationDomainService.baseDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.guidDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.supportedSearchFields = ['ldmName']
+        genericBasicValidationDomainService.decorator = GlobalUniqueIdentifier.class
+        List<GlobalUniqueIdentifier> globalUniqueIdentifierList = genericBasicValidationDomainService.list([ldmName: 'persons'])
+        assertNotNull(globalUniqueIdentifierList)
+        globalUniqueIdentifierList.each {
+            assertEquals('persons', it.ldmName)
+        }
+    }
+
+    @Test
+    void testListSameDomainFilteredWhenEthosNameAndDomainFieldNameIsDifferent() {
+        genericBasicValidationDomainService.baseDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.guidDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.decorator = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.supportedSearchFields = ['resourceType']
+        genericBasicValidationDomainService.ethosToDomainFieldNameMap = ['resourceType': 'ldmName']
+        List<GlobalUniqueIdentifier> globalUniqueIdentifierList = genericBasicValidationDomainService.list(['resourceType': 'persons'])
+        assertNotNull(globalUniqueIdentifierList)
+        globalUniqueIdentifierList.each {
+            assertEquals('persons', it.ldmName)
+        }
+    }
+
+    @Test
+    void testListSameDomainFilteredWhenEthosNameAndDomainFieldNameIsDifferentInvalidMapping() {
+        genericBasicValidationDomainService.baseDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.guidDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.decorator = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.supportedSearchFields = ['resourceType']
+        genericBasicValidationDomainService.ethosToDomainFieldNameMap = ['resourceType1': 'ldmName']
+        shouldFail(HibernateQueryException) {
+            List<GlobalUniqueIdentifier> globalUniqueIdentifierList = genericBasicValidationDomainService.list(['resourceType': 'persons'])
+        }
+    }
+
+    @Test
+    void testListDifferentDomainNoFilters() {
+        genericBasicValidationDomainService.baseDomain = Level.class
+        genericBasicValidationDomainService.guidDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.decorator = AcademicLevel.class
+        genericBasicValidationDomainService.guidDomainKeyField = 'domainKey'
+        genericBasicValidationDomainService.ldmNameField = 'ldmName'
+        genericBasicValidationDomainService.ldmNameValue = 'academic-levels'
+        genericBasicValidationDomainService.baseDomainKeyField = 'code'
+        genericBasicValidationDomainService.defaultSortField = 'code'
+        List<AcademicLevel> levels = genericBasicValidationDomainService.list([:])
+        assertNotNull(levels)
+        levels.each {
+            assertTrue(it instanceof AcademicLevel)
+        }
+    }
+
+    @Test
+    void testListDifferentDomainFiltered() {
+        genericBasicValidationDomainService.baseDomain = Level.class
+        genericBasicValidationDomainService.guidDomain = GlobalUniqueIdentifier.class
+        genericBasicValidationDomainService.decorator = AcademicLevel.class
+        genericBasicValidationDomainService.guidDomainKeyField = 'domainKey'
+        genericBasicValidationDomainService.ldmNameField = 'ldmName'
+        genericBasicValidationDomainService.ldmNameValue = 'academic-levels'
+        genericBasicValidationDomainService.baseDomainKeyField = 'code'
+        genericBasicValidationDomainService.defaultSortField = 'code'
+        genericBasicValidationDomainService.supportedSearchFields = ['ceuInd']
+        List<AcademicLevel> levels = genericBasicValidationDomainService.list(['ceuInd':true])
+        assertNotNull(levels)
+        levels.each {
+            assertTrue(it instanceof AcademicLevel)
+            assertEquals(true, it.ceuInd)
+        }
+    }
+}

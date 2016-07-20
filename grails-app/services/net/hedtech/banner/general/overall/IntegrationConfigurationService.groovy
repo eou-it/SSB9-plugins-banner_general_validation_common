8e8ab83be5a2510c5702d4e8d9908611534cb0d6
Arihant Chhajer <Arihant.Chhajer@ellucian.com>
2016-07-20 17:54:47 +0530

    Educational Institution API implemented

--- grails-app/services/net/hedtech/banner/general/overall/IntegrationConfigurationService.groovy
+++ grails-app/services/net/hedtech/banner/general/overall/IntegrationConfigurationService.groovy
@@ -6,6 +6,8 @@ package net.hedtech.banner.general.overall
 import net.hedtech.banner.exceptions.ApplicationException
 import net.hedtech.banner.exceptions.BusinessLogicValidationException
 import net.hedtech.banner.service.ServiceBase
+import net.hedtech.banner.general.system.ldm.HedmAddressType
+import net.hedtech.banner.general.common.GeneralValidationCommonConstants
 
 class IntegrationConfigurationService extends  ServiceBase {
 
@@ -13,6 +15,7 @@ class IntegrationConfigurationService extends  ServiceBase {
 
     static final String PROCESS_CODE = "HEDM"
     static final String NATION_ISO = "NATION.ISOCODE"
+    static final String ADDRESSES_DEFAULT_ADDRESSTYPE = "ADDRESSES.DEFAULT.ADDRESSTYPE"
 
     public boolean isInstitutionUsingISO2CountryCodes(){
         IntegrationConfiguration intConfig= IntegrationConfiguration.fetchAllByProcessCodeAndSettingName(PROCESS_CODE, NATION_ISO)[0]
@@ -29,4 +32,18 @@ class IntegrationConfigurationService extends  ServiceBase {
             throw new ApplicationException('Nation ISOCODE', new BusinessLogicValidationException('goriccr.invalid.value.message', [NATION_ISO]))
         }
     }
+
+    public String getDefaultAddressTypeV6() {
+        IntegrationConfiguration intConf = IntegrationConfiguration.findByProcessCodeAndSettingName(PROCESS_CODE, ADDRESSES_DEFAULT_ADDRESSTYPE)
+        if (!intConf) {
+            throw new ApplicationException("Default Address", new BusinessLogicValidationException("goriccr.not.found.message", [ADDRESSES_DEFAULT_ADDRESSTYPE]))
+        }
+        HedmAddressType hedmAddressType = HedmAddressType.getByString(intConf.value, GeneralValidationCommonConstants.VERSION_V6)
+        if (!hedmAddressType) {
+            throw new ApplicationException("Default Address", new BusinessLogicValidationException("goriccr.invalid.value.message", [ADDRESSES_DEFAULT_ADDRESSTYPE]))
+        }
+
+        return intConf.value
+    }
+
 }

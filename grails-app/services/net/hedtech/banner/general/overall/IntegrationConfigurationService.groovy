5a4379ab84bbb98ad4dd0ce845f022210dd0ac5a
Manjunath Muniraju <manjunath.muniraju@ellucian.com>
2016-07-20 18:16:02 +0530

    Adding new method to get default country code from goriccr

--- grails-app/services/net/hedtech/banner/general/overall/IntegrationConfigurationService.groovy
+++ grails-app/services/net/hedtech/banner/general/overall/IntegrationConfigurationService.groovy
@@ -5,6 +5,7 @@ package net.hedtech.banner.general.overall
 
 import net.hedtech.banner.exceptions.ApplicationException
 import net.hedtech.banner.exceptions.BusinessLogicValidationException
+import net.hedtech.banner.general.utility.IsoCodeService
 import net.hedtech.banner.service.ServiceBase
 import net.hedtech.banner.general.system.ldm.HedmAddressType
 import net.hedtech.banner.general.common.GeneralValidationCommonConstants
@@ -13,9 +14,12 @@ class IntegrationConfigurationService extends  ServiceBase {
 
     boolean transactional = true
 
+    IsoCodeService isoCodeService
+
     static final String PROCESS_CODE = "HEDM"
     static final String NATION_ISO = "NATION.ISOCODE"
     static final String ADDRESSES_DEFAULT_ADDRESSTYPE = "ADDRESSES.DEFAULT.ADDRESSTYPE"
+    static final String COUNTRY_DEFAULT_ISO = "ADDRESS.COUNTRY.DEFAULT"
 
     public boolean isInstitutionUsingISO2CountryCodes(){
         IntegrationConfiguration intConfig= IntegrationConfiguration.fetchAllByProcessCodeAndSettingName(PROCESS_CODE, NATION_ISO)[0]
@@ -44,6 +48,20 @@ class IntegrationConfigurationService extends  ServiceBase {
         }
 
         return intConf.value
+    }
+
+
+    public String getDefaultISO3CountryCodeForAddress() {
+        IntegrationConfiguration intConfig= IntegrationConfiguration.fetchAllByProcessCodeAndSettingName(PROCESS_CODE, COUNTRY_DEFAULT_ISO)[0]
+        if (!intConfig) {
+            throw new ApplicationException('Country ISOCODE', new BusinessLogicValidationException("goriccr.not.found.message", [COUNTRY_DEFAULT_ISO]))
+        }
+        if (intConfig.value && isoCodeService.getISO2CountryCode(intConfig.value)){
+            return intConfig.value
+        }
+        else {
+            throw new ApplicationException('Country ISOCODE', new BusinessLogicValidationException('goriccr.invalid.value.message', [COUNTRY_DEFAULT_ISO]))
+        }
     }
 
 }

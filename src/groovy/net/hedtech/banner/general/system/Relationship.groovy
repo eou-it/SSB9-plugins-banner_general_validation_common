2e0c1ec49d266de4ac975acfed7fda2f3d32f359
Sumeet Yajnik <sumeet.yajnik@ellucian.com>
2016-08-24 15:01:07 +0530

    BAPI-6277:: added business logic to implemention relationship type in  person contacts HEDM API

--- src/groovy/net/hedtech/banner/general/system/Relationship.groovy
+++ src/groovy/net/hedtech/banner/general/system/Relationship.groovy
@@ -3,19 +3,7 @@
  ****************************************************************************** */
 package net.hedtech.banner.general.system
 
-import javax.persistence.Column
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
-import javax.persistence.NamedQueries
-import javax.persistence.NamedQuery
-import javax.persistence.SequenceGenerator
-import javax.persistence.Table
-import javax.persistence.Temporal
-import javax.persistence.TemporalType
-import javax.persistence.Version
-import org.hibernate.annotations.Type
+import javax.persistence.*
 
 /**
  * Relationship Validation Table

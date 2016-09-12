134e49d2f91b57166a0447b5e18eb55894e3a2ec
Jim Caley <jim.caley@ellucian.com>
2016-09-12 13:56:32 -0400

    Implement BXEGS-70 "Fetch phone type for select box"

--- grails-app/services/net/hedtech/banner/general/system/TelephoneTypeService.groovy
+++ grails-app/services/net/hedtech/banner/general/system/TelephoneTypeService.groovy
@@ -3,6 +3,7 @@
  *******************************************************************************/
 package net.hedtech.banner.general.system
 
+import net.hedtech.banner.exceptions.ApplicationException
 import net.hedtech.banner.service.ServiceBase
 
 /**
@@ -18,5 +19,49 @@ class TelephoneTypeService extends ServiceBase {
      */
       TelephoneType fetchByCode(String code){
         return TelephoneType.fetchByCode(code)
+    }
+
+    /**
+     * Fetch TelephoneType details for updatable UI select list (read a Select2 widget).
+     * @param max
+     * @param offset
+     * @param searchString
+     * @return List of TelephoneType objects
+     */
+    def fetchUpdateableTelephoneTypeList(int max = 10, int offset = 0, String searchString = '') {
+        def telephoneTypeList = super.list(sort: "description", order: "asc")
+        def results
+
+
+        if(telephoneTypeList.size() == 0) {
+            throw new ApplicationException(TelephoneTypeService, "@@r1:noTelephoneTypesAvailable@@")
+        }
+
+        telephoneTypeList = telephoneTypeList.sort {
+            it.description
+        }
+
+        if(searchString.size() > 0) {
+            results = telephoneTypeList.findAll {
+                it.description.find(/(?i)$searchString/)
+            }
+        }
+        else {
+            results = telephoneTypeList
+        }
+
+        if(offset >= results.size()) {
+            results = []
+        }
+        else {
+            results = results.subList(offset, results.size())
+        }
+
+        if(max >= results.size()) {
+            return results
+        }
+        else {
+            return results.subList(0, max)
+        }
     }
 }

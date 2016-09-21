225ed7be2df941ab15bfdf73bd4e36c204b54c55
Jim Caley <jim.caley@ellucian.com>
2016-09-21 16:47:36 -0400

    Implement BXEGS-352 "Create emergency contact - BACKEND"

---
+++ grails-app/services/net/hedtech/banner/general/system/RelationshipService.groovy
@@ -0,0 +1,29 @@
+package net.hedtech.banner.general.system
+
+import net.hedtech.banner.exceptions.ApplicationException
+import net.hedtech.banner.service.ServiceBase
+
+class RelationshipService extends ServiceBase {
+
+    // TODO: implement as part of BXEGS-86 "Fetch relationship types for select list"
+//    def fetchRelationshipList(int max = 10, int offset = 0, String searchString = '') {
+//        def criteria = Relationship.createCriteria()
+//        def relationshipList = criteria.list(max: max, offset: offset, sort: 'description', order: 'asc') {
+//            and {
+//                ilike("description", "%${searchString}%")
+//            }
+//        }
+//
+//        relationshipList
+//    }
+
+    def fetchRelationship(def code) {
+        def relationship = Relationship.fetchByCode(code)
+
+        if(!relationship){
+            throw new ApplicationException(County, "@@r1:invalidRelationship@@")
+        }
+
+        relationship
+    }
+}

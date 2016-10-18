df6afde1e5781449176d6b1aad17f46a72a9532d
Alka Singh <alka.singh@ellucian.com>
2016-10-18 16:15:01 +0530

    BAPI-6538 - Persons V6 API POST and PUT -Phase III

--- grails-app/services/net/hedtech/banner/general/system/CitizenTypeService.groovy
+++ grails-app/services/net/hedtech/banner/general/system/CitizenTypeService.groovy
@@ -1,9 +1,28 @@
 package net.hedtech.banner.general.system
 
+import net.hedtech.banner.general.common.GeneralValidationCommonConstants
+import net.hedtech.banner.general.overall.ldm.GlobalUniqueIdentifier
+import net.hedtech.banner.general.overall.ldm.GlobalUniqueIdentifierService
 import net.hedtech.banner.service.ServiceBase
 
 class CitizenTypeService extends ServiceBase {
 
+    GlobalUniqueIdentifierService globalUniqueIdentifierService
+
     boolean transactional = true
+
+    public def fetchByGuid(String guid){
+        CitizenType citizenType
+        GlobalUniqueIdentifier globalUniqueIdentifier = globalUniqueIdentifierService.fetchByLdmNameAndGuid(GeneralValidationCommonConstants.CITIZENSHIP_STATUSES_LDM_NAME, guid?.toLowerCase()?.trim())
+        if(globalUniqueIdentifier){
+            citizenType = CitizenType.findByCode(globalUniqueIdentifier.domainKey)
+        }
+        return citizenType
+    }
+
+    public def fetchByCitizenIndicator(Boolean CitizenIndicator) {
+        return CitizenType.findByCitizenIndicator(CitizenIndicator)
+    }
+
 
 }

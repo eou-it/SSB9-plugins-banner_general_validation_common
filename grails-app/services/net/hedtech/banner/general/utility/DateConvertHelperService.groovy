5a0bdcb4031033ff85443193daaa11c661d62101
Vijay Kumar Tiwari <tiwari.vijay.k@gmail.com>
2017-01-23 16:57:40 +0530

    Adding a workaround for handing UTC dates from eedm in date conversion

--- grails-app/services/net/hedtech/banner/general/utility/DateConvertHelperService.groovy
+++ grails-app/services/net/hedtech/banner/general/utility/DateConvertHelperService.groovy
@@ -28,9 +28,13 @@ class DateConvertHelperService {
         if(!date){
             return null
         }
-        DateFormat dateFormat = new SimpleDateFormat(GeneralValidationCommonConstants.UTC_DATE_FORMAT);
-        dateFormat.setTimeZone(TimeZone.getTimeZone(GeneralValidationCommonConstants.UTC_TIME_ZONE));
-        return dateFormat.format(date);
+        //we are not doing proper date conversion using dateformatter as banner does not have timezone
+        //storage capability due to which we loose the timezone info
+        //and dates change between what is saved and what is read
+        //we only convert the date part and simply attach 'T'00:00:00+00:00 to denote UTC
+        DateFormat dateFormat = new SimpleDateFormat(GeneralValidationCommonConstants.DATE_WITHOUT_TIMEZONE);
+        //dateFormat.setTimeZone(TimeZone.getTimeZone(GeneralValidationCommonConstants.UTC_TIME_ZONE));
+        return dateFormat.format(date)+"'T'00:00:00+00:00";
     }
 
     /**
@@ -82,6 +86,10 @@ class DateConvertHelperService {
         if(!utc){
             return null
         }
-        return new SimpleDateFormat(GeneralValidationCommonConstants.UTC_DATE_FORMAT).parse(utc)
+          //we are not doing proper date conversion using dateformatter as banner does not have timezone
+          //storage capability due to which we loose the timezone info
+          //and dates change between what is saved and what is read
+          //we only parse the date component from the given object
+        return new SimpleDateFormat(GeneralValidationCommonConstants.DATE_WITHOUT_TIMEZONE).parse(utc)
     }
 }

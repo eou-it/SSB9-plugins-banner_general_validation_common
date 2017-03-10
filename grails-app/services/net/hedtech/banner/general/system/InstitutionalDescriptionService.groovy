8add46d3e370dba8bbd9b73b9a0b104f5ad045f0
Venkat Koppisetty <venkat.koppisetty@ellucian.com>
2017-03-10 14:10:33 +0530

    Added InstitutionDescriptionService.getISO4217CurrencyCodeForInstitutionBaseCurrency method

--- grails-app/services/net/hedtech/banner/general/system/InstitutionalDescriptionService.groovy
+++ grails-app/services/net/hedtech/banner/general/system/InstitutionalDescriptionService.groovy
@@ -3,17 +3,38 @@
  ****************************************************************************** */
 package net.hedtech.banner.general.system
 
+import net.hedtech.banner.exceptions.ApplicationException
+import net.hedtech.banner.exceptions.BusinessLogicValidationException
+import net.hedtech.banner.general.utility.IsoCodeService
 import net.hedtech.banner.service.ServiceBase
 
 /**
- * A transactional service supporting persistence of the InstitutionalDescriptionService model. 
+ * A transactional service supporting persistence of the InstitutionalDescriptionService model.
  * */
-class InstitutionalDescriptionService extends ServiceBase{
+class InstitutionalDescriptionService extends ServiceBase {
 
     boolean transactional = true
+
+    CurrencyConversionService currencyConversionService
+    IsoCodeService isoCodeService
 
 
     def InstitutionalDescription findByKey() {
         return InstitutionalDescription.fetchByKey()
     }
+
+
+    String getInstitutionBaseCurrencyCode() {
+        return findByKey().baseCurrCode
+    }
+
+
+    String getISO4217CurrencyCodeForInstitutionBaseCurrency() {
+        CurrencyConversion currencyConversion = currencyConversionService.findByCurrencyConversion(getInstitutionBaseCurrencyCode())
+        String isoCurrencyCode = currencyConversion?.standardCodeIso
+        if (!isoCurrencyCode || !isoCodeService.isValidISO4217CurrencyCode(isoCurrencyCode)) {
+            throw new ApplicationException(this.class, new BusinessLogicValidationException("currency.not.mapped", null))
+        }
+    }
+
 }

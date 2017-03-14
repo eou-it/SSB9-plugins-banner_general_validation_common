b9268cf587f2ede22ccb3c9ab4092ea8ba146e6f
Arihant Chhajer <Arihant.Chhajer@ellucian.com>
2017-03-14 19:51:47 +0530

    Added unit test file for Institutional Description Service

---
+++ test/integration/net/hedtech/banner/general/system/InstitutionalDescriptionServiceIntegrationTests.groovy
@@ -0,0 +1,29 @@
+/** *****************************************************************************
+ Copyright 2017 Ellucian Company L.P. and its affiliates.
+ ****************************************************************************** */
+package net.hedtech.banner.general.system
+
+import net.hedtech.banner.general.utility.IsoCodeService
+import net.hedtech.banner.testing.BaseIntegrationTestCase
+import org.junit.Before
+import org.junit.Test
+
+class InstitutionalDescriptionServiceIntegrationTests extends BaseIntegrationTestCase {
+
+    InstitutionalDescriptionService institutionalDescriptionService
+    IsoCodeService isoCodeService
+
+    @Before
+    public void setUp() {
+        formContext = ['GUAGMNU'] // Since we are not testing a controller, we need to explicitly set this
+        super.setUp()
+    }
+
+    @Test
+    void testGetISO4217CurrencyCodeForInstitutionBaseCurrency() {
+        def isoCurrencyCode = institutionalDescriptionService.getISO4217CurrencyCodeForInstitutionBaseCurrency()
+
+        assertNotNull isoCurrencyCode
+        assertTrue(isoCodeService.isValidISO4217CurrencyCode(isoCurrencyCode))
+    }
+}

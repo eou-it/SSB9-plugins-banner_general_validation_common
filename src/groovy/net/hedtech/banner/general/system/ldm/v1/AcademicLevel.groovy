b08654949ac17eb4f872c462c0e7d1ac8d1e4004
swaroopsg <swaroop.sg@ellucian.com>
2017-07-03 11:57:04 +0530

    fixed academic level and subject Api errors after movement

---
+++ src/groovy/net/hedtech/banner/general/system/ldm/v1/AcademicLevel.groovy
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ Copyright 2014-2016 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
+package net.hedtech.banner.general.system.ldm.v1
+
+import net.hedtech.banner.general.system.Level
+
+/**
+ * Decorator for Academic Level LDM (/base/domain/academicLevel/v1/academicLevel.json-schema)
+ *
+ */
+class AcademicLevel {
+
+    @Delegate
+    private final Level level
+    Metadata metadata
+    String guid
+
+    AcademicLevel(){
+        //used by GenericBasicValidationService
+        level = new Level()
+    }
+
+    AcademicLevel(Level level, String guid, Metadata metadata) {
+        this.level = level
+        this.guid = guid
+        this.metadata = metadata
+    }
+
+
+    boolean equals(o) {
+        if (this.is(o)) return true
+        if (getClass() != o.class) return false
+        AcademicLevel that = (AcademicLevel) o
+        if (level != that.level) return false
+        if (metadata != that.metadata) return false
+        if (guid != that.guid) return false
+        return true
+    }
+
+
+    public String toString() {
+        """AcademicLevel[
+                    level=$level,
+                    guid=$guid,
+                    metadata=$metadata]"""
+    }
+
+    public def getDetail(){
+        return ['id': guid]
+    }
+}
---
+++ src/groovy/net/hedtech/banner/general/system/ldm/v1/SubjectDetail.groovy
@@ -0,0 +1,58 @@
+/** *******************************************************************************
+ Copyright 2014-2015 Ellucian Company L.P. and its affiliates.
+ ********************************************************************************* */
+package net.hedtech.banner.general.system.ldm.v1
+
+import net.hedtech.banner.general.system.Subject
+
+
+/**
+ * LDM decorator for Subject  resource
+ * which is sent back to the consumer
+ */
+class SubjectDetail {
+
+
+    @Delegate
+    private final Subject subject
+    Metadata metadata
+    String guid
+
+
+    SubjectDetail(Subject subject, String guid, Metadata metadata) {
+        this.subject = subject
+        this.metadata = metadata
+        this.guid = guid
+    }
+
+    Subject getSubject(){
+        return subject
+    }
+
+    /**
+     * Equals method to compare the two SubjectDetail
+     * Objects
+     * @param o
+     * @return
+     */
+    boolean equals(o) {
+        if (this.is(o)) return true
+        if (getClass() != o.class) return false
+
+        SubjectDetail that = (SubjectDetail) o
+        if (metadata != that.metadata) return false
+        if (guid != that.guid) return false
+        if (subject != that.subject) return false
+
+        return true
+    }
+
+
+    public String toString() {
+        """SubjectDetail[
+                    subject=$subject,
+                    guid=$guid,
+                    metadata=$metadata]"""
+    }
+
+}

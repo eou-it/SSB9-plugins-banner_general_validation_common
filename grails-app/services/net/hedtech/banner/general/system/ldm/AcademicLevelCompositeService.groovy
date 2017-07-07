5fbc2932b9f4aac73f75e4df49d1f77e7cd062a6
swaroopsg <swaroop.sg@ellucian.com>
2017-07-07 11:44:02 +0530

    moved academiclevel composite service to general_vaidation_common

---
+++ grails-app/services/net/hedtech/banner/general/system/ldm/AcademicLevelCompositeService.groovy
@@ -0,0 +1,239 @@
+/*******************************************************************************
+ Copyright 2014-2017 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
+package net.hedtech.banner.general.system.ldm
+
+import net.hedtech.banner.exceptions.ApplicationException
+import net.hedtech.banner.exceptions.BusinessLogicValidationException
+import net.hedtech.banner.exceptions.NotFoundException
+import net.hedtech.banner.general.common.GeneralValidationCommonConstants
+import net.hedtech.banner.general.overall.ldm.GlobalUniqueIdentifier
+import net.hedtech.banner.general.overall.ldm.LdmService
+import net.hedtech.banner.general.system.Level
+import net.hedtech.banner.general.system.ldm.v1.AcademicLevel
+import net.hedtech.banner.general.system.ldm.v1.Metadata
+import net.hedtech.banner.restfulapi.RestfulApiValidationUtility
+import org.springframework.transaction.annotation.Transactional
+
+/**
+ * LevelCompositeService.
+ * This class supports Level (Course) service for LDM.
+ */
+
+@Transactional
+class AcademicLevelCompositeService extends LdmService {
+
+    def levelService
+
+    private static
+    final List<String> VERSIONS = [GeneralValidationCommonConstants.VERSION_V1, GeneralValidationCommonConstants.VERSION_V6]
+
+    /**
+     * GET /api/academic-levels
+     *
+     * @param params Request parameters
+     * @return
+     */
+    @Transactional(readOnly = true)
+    List<AcademicLevel> list(Map params) {
+        List academicLevels = []
+        RestfulApiValidationUtility.correctMaxAndOffset(params, RestfulApiValidationUtility.MAX_DEFAULT, RestfulApiValidationUtility.MAX_UPPER_LIMIT)
+        List allowedSortFields = (GeneralValidationCommonConstants.VERSION_V6.equals(getAcceptVersion(VERSIONS)) ? [GeneralValidationCommonConstants.CODE, GeneralValidationCommonConstants.TITLE] : [GeneralValidationCommonConstants.ABBREVIATION, GeneralValidationCommonConstants.TITLE])
+        if (params.containsKey("sort")) {
+            RestfulApiValidationUtility.validateSortField(params.sort, allowedSortFields)
+            params.sort = fetchBannerDomainPropertyForLdmField(params.sort)
+        }
+
+        if (params.containsKey("order")) {
+            RestfulApiValidationUtility.validateSortOrder(params.order)
+        } else {
+            params.put('order', "asc")
+        }
+
+        String sortField = params.sort?.trim()
+        String sortOrder = params.order?.trim()
+        int max = params.max?.trim()?.toInteger() ?: 0
+        int offset = params.offset?.trim()?.toInteger() ?: 0
+
+        Map mapForSearch = [:]
+        List<Level> levels = levelService.fetchAllByCriteria(mapForSearch, sortField, sortOrder, max, offset) as List
+
+        levels.each { level ->
+            academicLevels << getDecorator(level)
+        }
+        return academicLevels
+    }
+
+    /**
+     * GET /api/academic-levels
+     *
+     * The count method must return the total number of instances of the resource.
+     * It is used in conjunction with the list method when returning a list of resources.
+     * RestfulApiController will make call to "count" only if the "list" execution happens without any exception.
+     *
+     * @return
+     */
+    Long count() {
+        return levelService.count()
+    }
+
+    /**
+     * GET /api/academic-levels/<guid>
+     *
+     * @param guid
+     * @return
+     */
+    @Transactional(readOnly = true)
+    AcademicLevel get(String guid) {
+        getAcceptVersion(VERSIONS)
+
+        GlobalUniqueIdentifier globalUniqueIdentifier = GlobalUniqueIdentifier.fetchByLdmNameAndGuid(GeneralValidationCommonConstants.ACADEMIC_LEVEL_LDM_NAME, guid)
+
+        Level level = fetchLevelByGuid(guid)
+        if (!level) {
+            throw new ApplicationException(GeneralValidationCommonConstants.ACADEMIC_LEVEL, new NotFoundException())
+        }
+        return getDecorator(level, globalUniqueIdentifier.guid)
+    }
+
+    /**
+     * POST /api/academic-levels
+     *
+     * @param content Request body
+     */
+    AcademicLevel create(Map content) {
+        Level level = levelService.fetchByCode(content.code)
+        if (level) {
+            def parameterValue = GeneralValidationCommonConstants.VERSION_V6.equals(LdmService.getAcceptVersion(VERSIONS)) ? GeneralValidationCommonConstants.CODE : GeneralValidationCommonConstants.ABBREVIATION
+            throw new ApplicationException(GeneralValidationCommonConstants.ACADEMIC_LEVEL, new BusinessLogicValidationException(GeneralValidationCommonConstants.ERROR_MSG_CODE_EXISTS, [parameterValue]))
+        }
+        level = bindLevel(new Level(), content)
+        String levelGuid = content.guid?.trim()?.toLowerCase()
+        if (levelGuid) {
+            // Overwrite the GUID created by DB insert trigger, with the one provided in the request body
+            levelGuid = updateGuidValue(level.id, levelGuid, GeneralValidationCommonConstants.ACADEMIC_LEVEL_LDM_NAME)?.guid
+        } else {
+            levelGuid = globalUniqueIdentifierService.fetchByLdmNameAndDomainId(GeneralValidationCommonConstants.ACADEMIC_LEVEL_LDM_NAME, level.id)?.guid
+        }
+        return getDecorator(level, levelGuid)
+    }
+
+    /**
+     * PUT /api/academic-levels/<guid>
+     *
+     * @param content Request body
+     */
+    AcademicLevel update(content) {
+        String levelGuid = content.id?.trim()?.toLowerCase()
+        if (!levelGuid) {
+            throw new ApplicationException(GeneralValidationCommonConstants.ACADEMIC_LEVEL, new NotFoundException())
+        }
+        GlobalUniqueIdentifier globalUniqueIdentifier = globalUniqueIdentifierService.fetchByLdmNameAndGuid(GeneralValidationCommonConstants.ACADEMIC_LEVEL_LDM_NAME, levelGuid)
+        if (!globalUniqueIdentifier) {
+            if (!content.guid) {
+                content.guid = levelGuid
+            }
+            //Per strategy when a GUID was provided, the create should happen.
+            return create(content)
+        }
+
+        Level level = levelService.get(globalUniqueIdentifier.domainId)
+        if (!level) {
+            throw new ApplicationException(GeneralValidationCommonConstants.ACADEMIC_LEVEL, new NotFoundException())
+        }
+
+        // Should not allow to update LEVEL.code as it is read-only
+        if (level.code != content.code?.trim()) {
+            content.code = level.code
+        }
+        level = bindLevel(level, content)
+        return getDecorator(level, levelGuid)
+    }
+
+    private def bindLevel(Level level, Map content) {
+        bindData(level, content)
+        levelService.createOrUpdate(level)
+    }
+
+    private void bindData(domainModel, content) {
+        super.setDataOrigin(domainModel, content)
+        super.bindData(domainModel, content, [:])
+        domainModel.ceuInd = Boolean.FALSE
+    }
+
+    private def getDecorator(Level level, String levelGuid = null) {
+        if (level) {
+            if (!levelGuid) {
+                levelGuid = GlobalUniqueIdentifier.findByLdmNameAndDomainId(GeneralValidationCommonConstants.ACADEMIC_LEVEL_LDM_NAME, level.id)?.guid
+            }
+            return new AcademicLevel(level, levelGuid, new Metadata(level?.dataOrigin))
+        }
+    }
+
+    AcademicLevel fetchByLevelId(Long domainId) {
+        if (null == domainId) {
+            return null
+        }
+        Level level = levelService.get(domainId)
+        if (!level) {
+            return null
+        }
+        return new AcademicLevel(level, GlobalUniqueIdentifier.findByLdmNameAndDomainId(GeneralValidationCommonConstants.ACADEMIC_LEVEL_LDM_NAME, domainId)?.guid, new Metadata(level.dataOrigin))
+    }
+
+
+    AcademicLevel fetchByLevelCode(String levelCode) {
+        if (!levelCode) {
+            return null
+        }
+        Level level = Level.findByCode(levelCode)
+        if (!level) {
+            return null
+        }
+        return new AcademicLevel(level, GlobalUniqueIdentifier.findByLdmNameAndDomainId(GeneralValidationCommonConstants.ACADEMIC_LEVEL_LDM_NAME, level.id)?.guid, new Metadata(level.dataOrigin))
+    }
+
+
+    Level fetchLevelByGuid(String guid) {
+        Level level
+        GlobalUniqueIdentifier globalUniqueIdentifier = GlobalUniqueIdentifier.fetchByLdmNameAndGuid(GeneralValidationCommonConstants.ACADEMIC_LEVEL_LDM_NAME, guid)
+        if (globalUniqueIdentifier) {
+            level = levelService.get(globalUniqueIdentifier.domainId)
+        }
+        return level
+    }
+
+
+    List<AcademicLevel> fetchAllByLevelIdInList(List<String> ids) {
+        List<AcademicLevel> academicLevelList = []
+        if (ids) {
+            List<GlobalUniqueIdentifier> levelGuidList = GlobalUniqueIdentifier.fetchAllByGuidInList(ids.unique())
+            if (levelGuidList) {
+                List<Level> levelList = levelService.fetchAllByCodeInList(levelGuidList.domainKey)
+                Map levelCodeMap = [:]
+                levelList.each {
+                    levelCodeMap.put(it.code, it)
+                }
+
+                levelGuidList.each {
+                    academicLevelList << new AcademicLevel(levelCodeMap.get(it.domainKey), it.guid, new Metadata(levelCodeMap.get(it.domainKey).dataOrigin))
+                }
+            }
+        }
+        return academicLevelList
+    }
+
+    def getLevelCodeToGuidMap(Collection<String> codes) {
+        Map<String, String> codeToGuidMap = [:]
+        if (codes) {
+            List entities = levelService.fetchAllWithGuidByCodeInList(codes)
+            entities.each {
+                Level level = it.level
+                GlobalUniqueIdentifier globalUniqueIdentifier = it.globalUniqueIdentifier
+                codeToGuidMap.put(level.code, globalUniqueIdentifier.guid)
+            }
+        }
+        return codeToGuidMap
+    }
+
+}
---
+++ test/integration/net/hedtech/banner/general/system/ldm/AcademicLevelCompositeServiceIntegrationTests.groovy
@@ -0,0 +1,335 @@
+/** *******************************************************************************
+ Copyright 2014-2016 Ellucian Company L.P. and its affiliates.
+ ********************************************************************************* */
+package net.hedtech.banner.general.system.ldm
+
+import net.hedtech.banner.exceptions.ApplicationException
+import net.hedtech.banner.general.common.GeneralValidationCommonConstants
+import net.hedtech.banner.general.overall.ldm.GlobalUniqueIdentifier
+import net.hedtech.banner.general.system.Level
+import net.hedtech.banner.general.system.ldm.v1.AcademicLevel
+import net.hedtech.banner.restfulapi.RestfulApiValidationException
+import net.hedtech.banner.testing.BaseIntegrationTestCase
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * AcademicLevelCompositeServiceIntegrationTests.
+ *
+ * Date: 7/24/14
+ * Time: 4:33 PM
+ */
+class AcademicLevelCompositeServiceIntegrationTests extends BaseIntegrationTestCase {
+
+    def academicLevelCompositeService
+
+    Level i_success_level
+    def i_success_code = "TZ"
+    def i_success_description = "Test Description"
+    def i_success_academicIndicator = true
+    def i_success_continuingEducationIndicator = true
+    def i_success_systemRequiredIndicator = true
+    def i_success_voiceResponseMessageNumber = 1
+    def i_success_electronicDataInterchangeEquivalent = "TT"
+    def i_success_content
+    private static final String LDM_NAME = 'academic-levels'
+
+    @Before
+    public void setUp() {
+        formContext = ['GUAGMNU']
+        super.setUp()
+        initiializeDataReferences()
+    }
+
+
+    private void initiializeDataReferences() {
+        i_success_level = Level.findByCode('AP')
+        i_success_content = [code: 'SV',description:'Test Title', metadata: [dataOrigin: 'Banner']]
+    }
+
+    @Test
+    void testListWithoutPaginationParams() {
+        List academicLevels = academicLevelCompositeService.list([:])
+        assertNotNull academicLevels
+        assertFalse academicLevels.isEmpty()
+        assertTrue academicLevels.code.contains(i_success_level.code)
+    }
+
+    @Test
+    void testListWithPagination() {
+        def paginationParams = [max: '20', offset: '0']
+        List academicLevels = academicLevelCompositeService.list(paginationParams)
+        assertNotNull academicLevels
+        assertFalse academicLevels.isEmpty()
+        assertTrue academicLevels.code.contains(i_success_level.code)
+    }
+
+    @Test
+    void testCount() {
+        assertNotNull i_success_level
+        assertEquals Level.count(), academicLevelCompositeService.count()
+    }
+
+    @Test
+    void testGetInvalidGuid() {
+        try {
+            academicLevelCompositeService.get('Invalid-guid')
+        } catch (ApplicationException ae) {
+            assertApplicationException ae, "NotFoundException"
+        }
+    }
+
+
+    @Test
+    void testGetInvalidNonExistentAcademicLevel() {
+        Level level = newValidForCreateLevel()
+        save level
+        assertNotNull level.id
+        def academicLevel = academicLevelCompositeService.fetchByLevelId(level.id)
+        assertNotNull academicLevel.toString()
+        assertNotNull academicLevel.guid
+        assertEquals academicLevel.id, level.id
+
+        level.delete(flush: true)
+        assertNull level.get(academicLevel.id)
+
+        try {
+            academicLevelCompositeService.get(academicLevel.guid)
+        } catch (ApplicationException ae) {
+            assertApplicationException ae, "NotFoundException"
+        }
+    }
+
+    @Test
+    void testGet() {
+        def paginationParams = [max: '1', offset: '0']
+        List academicLevels = academicLevelCompositeService.list(paginationParams)
+        assertNotNull academicLevels
+        assertFalse academicLevels.isEmpty()
+
+        assertNotNull academicLevels[0].guid
+        def academicLevel = academicLevelCompositeService.get(academicLevels[0].guid)
+        assertNotNull academicLevel
+        assertEquals academicLevels[0].guid, academicLevel.guid
+        assertEquals academicLevels[0].code, academicLevel.code
+        assertEquals academicLevels[0].metadata, academicLevel.metadata
+        assertEquals academicLevels[0], academicLevel
+    }
+
+    @Test
+    void testFetchByAcademicLevelId() {
+        def academicLevel = academicLevelCompositeService.fetchByLevelId(i_success_level.id)
+        assertNotNull academicLevel
+        assertEquals i_success_level.id, academicLevel.id
+        assertEquals i_success_level.code, academicLevel.code
+        assertEquals i_success_level.description, academicLevel.description
+        assertEquals i_success_level.dataOrigin, academicLevel.metadata.dataOrigin
+    }
+
+    @Test
+    void testFetchByAcademicLevelIdInvalid() {
+        assertNull academicLevelCompositeService.fetchByLevelId(null)
+    }
+
+    @Test
+    void testFetchByAcademicLevel() {
+        def academicLevel = academicLevelCompositeService.fetchByLevelCode(i_success_level.code)
+        assertNotNull academicLevel
+        assertEquals i_success_level.id, academicLevel.id
+        assertEquals i_success_level.code, academicLevel.code
+        assertEquals i_success_level.description, academicLevel.description
+        assertEquals i_success_level.dataOrigin, academicLevel.metadata.dataOrigin
+    }
+
+    @Test
+    void testFetchByAcademicLevelInvalid() {
+        assertNull academicLevelCompositeService.fetchByLevelCode(null)
+
+        def invalidLevelCode = 'A1'
+        if (!Level.findByCode(invalidLevelCode)){
+            assertNull academicLevelCompositeService.fetchByLevelCode(invalidLevelCode)
+        }
+    }
+
+    /**
+     * Test to check the AcademicLevelCompositeService list method with valid sort and order field and supported version
+     * If No "Accept" header is provided, by default it takes the latest supported version
+     */
+    @Test
+    void testListWithValidSortAndOrderFieldWithSupportedVersion() {
+        def params = [order: 'ASC', sort: 'code']
+        def academicLevelList = academicLevelCompositeService.list(params)
+        assertNotNull academicLevelList
+        assertFalse academicLevelList.isEmpty()
+        assertTrue academicLevelList.code.contains(i_success_level.code)
+    }
+
+
+    /**
+     * Test to check the AcademicLevelCompositeService list method with invalid order field
+     */
+    @Test
+    void testListWithInvalidSortOrder() {
+        shouldFail(RestfulApiValidationException) {
+            def map = [order: 'test']
+            academicLevelCompositeService.list(map)
+        }
+    }
+
+    /**
+     * Test to check the AcademicLevelCompositeService list method with invalid sort field
+     */
+    @Test
+    void testListWithInvalidSortField() {
+        shouldFail(RestfulApiValidationException) {
+            def map = [sort: 'test']
+            academicLevelCompositeService.list(map)
+        }
+    }
+
+
+    /**
+     * Test to check the sort by code on AcademicLevelCompositeService
+     * */
+    @Test
+    public void testSortByCode(){
+        params.order='ASC'
+        params.sort='code'
+        List list = academicLevelCompositeService.list(params)
+        assertNotNull list
+        def tempParam=null
+        list.each{
+            academicLevel->
+                String code=academicLevel.code
+                if(!tempParam){
+                    tempParam=code
+                }
+                assertTrue tempParam.compareToIgnoreCase(code)<0 || tempParam.compareToIgnoreCase(code)==0
+                tempParam=code
+        }
+
+        params.clear()
+        params.order='DESC'
+        params.sort='code'
+        list = academicLevelCompositeService.list(params)
+        assertNotNull list
+        tempParam=null
+        list.each{
+            academicLevel->
+                String code=academicLevel.code
+                if(!tempParam){
+                    tempParam=code
+                }
+                assertTrue tempParam.compareToIgnoreCase(code)>0 || tempParam.compareToIgnoreCase(code)==0
+                tempParam=code
+        }
+    }
+
+    /**
+     * Test to check the AcademicLevelCompositeService create method with valid in request content
+     */
+    @Test
+    void testCreate() {
+        def academicLevel = academicLevelCompositeService.create(i_success_content)
+        assertNotNull academicLevel
+        assertNotNull academicLevel.guid
+        assertEquals i_success_content.code, academicLevel.code
+        assertEquals i_success_content.description, academicLevel.description
+    }
+
+    /**
+     * Test to check the AcademicLevelCompositeService create method with exists code in request content
+     */
+    @Test
+    void testCreateExistsCode(){
+        i_success_content.code='LW'
+        try{
+            academicLevelCompositeService.create(i_success_content)
+        }catch (ApplicationException ae){
+            assertApplicationException ae, "code.exists.message"
+        }
+    }
+
+    /**
+     * Test to check the AcademicLevelCompositeService update method with valid guid in request content
+     */
+    @Test
+    void testUpdate() {
+        GlobalUniqueIdentifier  globalUniqueIdentifier=  GlobalUniqueIdentifier.findByLdmName(LDM_NAME)
+        assertNotNull globalUniqueIdentifier
+        i_success_content.put('id', globalUniqueIdentifier?.guid)
+        def academicLevel = academicLevelCompositeService.update(i_success_content)
+        assertNotNull academicLevel
+        assertNotNull academicLevel.guid
+        assertEquals globalUniqueIdentifier?.domainKey, academicLevel.code
+        assertEquals i_success_content.description, academicLevel.description
+    }
+
+    /**
+     * Test to check the AcademicLevelCompositeService update method with invalid guid in request content
+     */
+    @Test
+    void testUpdateNullGuid() {
+        i_success_content.put('id', null)
+        try{
+            academicLevelCompositeService.update(i_success_content)
+        }
+        catch (ApplicationException ae) {
+            assertApplicationException ae, "NotFoundException"
+        }
+    }
+
+
+    /**
+     * Test to check the AcademicLevelCompositeService update method with non exists guid in request content
+     */
+    @Test
+    void testUpdateNonExistsGuid() {
+        i_success_content.put('id', 'TEST')
+        def academicLevel = academicLevelCompositeService.update(i_success_content)
+        assertNotNull academicLevel
+        assertNotNull academicLevel.guid
+        assertEquals i_success_content.id?.trim()?.toLowerCase(), academicLevel.guid
+        assertEquals i_success_content.code, academicLevel.code
+        assertEquals i_success_content.description, academicLevel.description
+    }
+
+    @Test
+    void testFetchAllByLevelIdInListNullList(){
+        assertEquals([],academicLevelCompositeService.fetchAllByLevelIdInList(null))
+    }
+
+    @Test
+    void testFetchAllByLevelIdInListEmptyList(){
+        assertEquals([],academicLevelCompositeService.fetchAllByLevelIdInList([]))
+    }
+
+    @Test
+    void testFetchAllByLevelIdInList(){
+        List<String> ids = GlobalUniqueIdentifier.findAllByLdmName([max:10],GeneralValidationCommonConstants.ACADEMIC_LEVEL_LDM_NAME)?.guid
+        List<AcademicLevel> academicLevelList = academicLevelCompositeService.fetchAllByLevelIdInList(ids)
+        assertEquals(ids.sort(), academicLevelList.guid.sort())
+    }
+
+    private def newValidForCreateLevel() {
+        def level = new Level(
+                code: i_success_code,
+                description: i_success_description,
+                acadInd: i_success_academicIndicator,
+                ceuInd: i_success_continuingEducationIndicator,
+                systemReqInd: i_success_systemRequiredIndicator,
+                vrMsgNo: i_success_voiceResponseMessageNumber,
+                ediEquiv: i_success_electronicDataInterchangeEquivalent
+        )
+        return level
+    }
+
+
+    @Test
+    void testGetLevelCodeToGuidMap(){
+        List levelCodes = [i_success_level.code]
+        def codeToGuidMap = academicLevelCompositeService.getLevelCodeToGuidMap(levelCodes)
+        assertTrue(codeToGuidMap.containsKey(i_success_level.code))
+    }
+
+}

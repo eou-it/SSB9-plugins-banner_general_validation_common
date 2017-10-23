b904a37c1dd6763baa0c667bc41be080c6fb781d
Kamalakannan Murugan <Kamalakannan.Murugan@ellucian.com>
2017-10-23 17:07:25 +0530

    version 12 constant added

--- grails-app/services/net/hedtech/banner/general/common/GeneralValidationCommonConstants.groovy
+++ grails-app/services/net/hedtech/banner/general/common/GeneralValidationCommonConstants.groovy
@@ -40,6 +40,7 @@ class GeneralValidationCommonConstants {
     final static String VERSION_V9 = 'v9'
     final static String VERSION_V10 = 'v10'
     final static String VERSION_V11 = 'v11'
+    final static String VERSION_V12 = 'v12'
     final static String ABBREVIATION = 'abbreviation'
     final static String TITLE = 'title'
     final static String RACE = 'race'

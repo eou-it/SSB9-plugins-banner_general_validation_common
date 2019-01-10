6122beb9b942ed9e81f9df57ee8644daa0b62565
Manjunath Muniraju <manjunath.muniraju@ellucian.com>
2019-01-10 10:55:36 +0530

    Copyrights update for state artifacts

--- test/integration/net/hedtech/banner/general/system/StateIntegrationTests.groovy
+++ test/integration/net/hedtech/banner/general/system/StateIntegrationTests.groovy
@@ -1,5 +1,5 @@
 /** *****************************************************************************
- Copyright 2009-2013 Ellucian Company L.P. and its affiliates.
+ Copyright 2009-2019 Ellucian Company L.P. and its affiliates.
  ****************************************************************************** */
 package net.hedtech.banner.general.system
 import org.junit.Before

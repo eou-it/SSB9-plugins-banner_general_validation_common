fa516633f90dc34d9e2798922178ec9a2fddbb78
Mauricio Abisay Lopez Velazquez <mauricio.lopez@ellucian.com>
2020-06-23 01:49:58 -0500

    MultiCurrency restore related chages.

--- grails-app/services/net/hedtech/banner/general/system/CurrencyConversionService.groovy
+++ grails-app/services/net/hedtech/banner/general/system/CurrencyConversionService.groovy
@@ -1,5 +1,5 @@
 /*******************************************************************************
-Copyright 2017 Ellucian Company L.P. and its affiliates.
+Copyright 2020 Ellucian Company L.P. and its affiliates.
 *******************************************************************************/
 package net.hedtech.banner.general.system
 
@@ -44,6 +44,20 @@ class CurrencyConversionService extends ServiceBase {
             }
         }
         return entity
+    }
+
+    /**
+     * Check the multi-currency feature enabled status
+     * @return true, if the multi-currency feature is enabled. false, if the multi-currency feature is disabled
+     */
+    static boolean isMultiCurrencyEnabled() {
+        boolean isMultiCurrencyEnabled = false
+        isMultiCurrencyEnabled = CurrencyConversion.withSession { org.hibernate.Session session ->
+            org.hibernate.SQLQuery multiCurrencyQuery = session.createSQLQuery("SELECT COUNT(TBBCTRL_MCTX_IND) FROM TBBCTRL WHERE TBBCTRL_MCTX_IND = 'Y'")
+            int counter = (int) multiCurrencyQuery.uniqueResult()
+            return (counter > 0)
+        }
+        return isMultiCurrencyEnabled
     }
 
 }

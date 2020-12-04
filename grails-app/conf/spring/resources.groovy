3a1da393fe56c3dbd58deb8b2aaba65d6fdf1cdf
Kyle Patrick <Kyle.Patrick@ellucian.com>
2020-12-04 14:17:41 -0500

    Corrected integration tests hanging

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -7,13 +7,5 @@
  * Spring bean configuration using Groovy DSL, versus normal Spring XML.
  */
 beans = {
-
-    // Wiring for plugin integration tests only.
-    ldmService(LdmService) {
-        grailsWebDataBinder = ref('grailsWebDataBinder')
-        sessionFactory = ref('sessionFactory')
-        globalUniqueIdentifierService = ref('globalUniqueIdentifierService')
-    }
-    
 }
 
